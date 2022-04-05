6b569779d3b88de255b298fb70da9c10e6600338
Malvino Austin Tanura <malvinoaustintanura@gmail.com>
2022-04-05 12:04:09 +0700

    Mengupload tugas Deployment

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Selamat Datang di Website Static Malvino Austin Tanura.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/MalvinoTan"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Saya
         </a>
       </header>
     </div>
